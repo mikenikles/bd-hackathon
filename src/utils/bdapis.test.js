40627160b57e3dfb59f7b7694ea5fca3cc567dfc
Drew Lau <drew@mobify.com>
2016-09-17 15:37:51 -0700

    Fix API call with CORS and json()

--- src/utils/bdapis.test.js
+++ src/utils/bdapis.test.js
@@ -4,6 +4,7 @@ import {
 
 it('load a product', async () => {
   // make request
-  const product = await loadProduct('10082137')
-  expect(product).toBeTruthy()
+  const product = await loadProduct('10082137');
+  expect(product).not.toBeNull();
+  expect(product.success).toBeTruthy();
 })
