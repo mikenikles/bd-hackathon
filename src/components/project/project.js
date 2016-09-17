2d4c61ff859ba3a404995a926b13ec9eabd5efa9
Maggie Caspar <mcaspar@mobify.com>
2016-09-17 15:57:25 -0700

    flesh out project page, add button styling

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -19,7 +19,7 @@ class Project extends Component {
 
     return (
       <div className="project">
-        <section>
+        <section className="c-steps">
             <Link to="/">Home</Link>
             <h1>title</h1>
             <p>Intent paragraph</p>
@@ -36,9 +36,10 @@ class Project extends Component {
                     <div className="c-attribute__item"></div>
                     <span>Budget</span>
                 </div>
+                <button className="c-button c--primary c--full-width">Lutton</button>
             </div>
         </section>
-        <section>
+        <section className="c-steps">
             <h2>1. Research &amp; Ideation</h2>
             <p>paragraph text</p>
             <div className="c-block">
@@ -49,26 +50,25 @@ class Project extends Component {
             </div>
             <h4>Notes</h4>
             <p>A bunch of notes</p>
+            <hr></hr>
         </section>
-        <section>
+        <section className="c-steps">
             <h2>Decisions</h2>
             <p>paragraph text</p>
             <h4>What I need</h4>
             <input type="checkbox" id="products" value="products"></input> <label for="cbox1">Products</label>
             <input type="checkbox" id="tasks" value="tasks"></input> <label for="cbox2">Tasks</label>
+            <hr></hr>
         </section>
-        <section>
-            <h2>3. Chart your progress</h2>
-            
-        </section>
+
         <section className="c-timeline">
-        <p>
-          {
-            timelineEntries.map((timelineEntry) => {
-              return <TimelineEntry {...timelineEntry}/>
-            })
-          }
-        </p>
+            <div>
+              {
+                timelineEntries.map((timelineEntry) => {
+                  return <TimelineEntry {...timelineEntry}/>
+                })
+              }
+            </div>
         </section>
       </div>
     );
