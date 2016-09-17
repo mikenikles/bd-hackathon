0499e43113c0edd5065342339e7b93a825f9d4e0
Helen <helen@mobify.com>
2016-09-17 12:11:37 -0700

    example routing to about

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router'
 import App from './components/App/App';
+import About from './components/About/About';
 import './index.scss';
 
 ReactDOM.render(
   <Router history={browserHistory}>
     <Route path="/" component={App}/>
+    <Route path="/about" component={About}/>
   </Router>,
   document.getElementById('root')
 );
