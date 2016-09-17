4acafdcd8ebb95c662c9bade52cd84b7a2788046
Maggie Caspar <mcaspar@mobify.com>
2016-09-17 15:28:48 -0700

    fleshing out project page

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './login.scss';
+import '../../index.scss';
 
 import { Link } from 'react-router'
 
--- src/components/project/project.js
+++ src/components/project/project.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './project.scss';
 import TimelineEntry from './timeline-entry'
+import '../../index.scss';
 
 import { Link } from 'react-router'
 
@@ -17,30 +18,50 @@ class Project extends Component {
     }]
 
     return (
-      <div className="t-project">
-        <Link to="/">Home</Link>
-        <h1>title</h1>
-        <p>Intent paragraph</p>
-        <div class="c-attribute">
-            <div>
-                <div class="c-attribute__item"></div>
-                <span>Skill Level</span>
+      <div className="project">
+        <section>
+            <Link to="/">Home</Link>
+            <h1>title</h1>
+            <p>Intent paragraph</p>
+            <div className="c-attribute">
+                <div>
+                    <div className="c-attribute__item"></div>
+                    <span>Skill Level</span>
+                </div>
+                <div>
+                    <div className="c-attribute__item"></div>
+                    <span>Time</span>
+                </div>
+                <div>
+                    <div className="c-attribute__item"></div>
+                    <span>Budget</span>
+                </div>
             </div>
-            <div>
-                <div class="c-attribute__item"></div>
-                <span>Time</span>
+        </section>
+        <section>
+            <h2>1. Research &amp; Ideation</h2>
+            <p>paragraph text</p>
+            <div className="c-block">
+                <h4>Inspirations</h4>
             </div>
-            <div>
-                <div class="c-attribute__item"></div>
-                <span>Budget</span>
+            <div className="c-block">
+                <h4>Design</h4>
             </div>
-        </div>
-        <h2>1. Research &amp; Ideation</h2>
-        <p>paragraph text</p>
-        
-
-
-
+            <h4>Notes</h4>
+            <p>A bunch of notes</p>
+        </section>
+        <section>
+            <h2>Decisions</h2>
+            <p>paragraph text</p>
+            <h4>What I need</h4>
+            <input type="checkbox" id="products" value="products"></input> <label for="cbox1">Products</label>
+            <input type="checkbox" id="tasks" value="tasks"></input> <label for="cbox2">Tasks</label>
+        </section>
+        <section>
+            <h2>3. Chart your progress</h2>
+            
+        </section>
+        <section className="c-timeline">
         <p>
           {
             timelineEntries.map((timelineEntry) => {
@@ -48,6 +69,7 @@ class Project extends Component {
             })
           }
         </p>
+        </section>
       </div>
     );
   }
