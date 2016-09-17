dc53069c264eac3c639cf53e254b44d59348cee8
Helen <helen@mobify.com>
2016-09-17 15:42:50 -0700

    pull data from firebase for project page

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import getRebase from '../../utils/rebase'
 import './project.scss';
 import TimelineEntry from './timeline-entry'
 
 import { Link } from 'react-router'
 
 class Project extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      projects: []
+    }
+  }
+
+  componentDidMount(){
+    getRebase().syncState(`projects`, {
+      context: this,
+      state: 'projects',
+      asArray: true
+    })
+  }
+
   render() {
+    if (this.state.projects.length === 0) {
+      return false
+    }
+
+    const project = this.state.projects[this.props.location.query.id]
+
     const timelineEntries = [{
       timestamp: 'September 17, 2016',
       content: 'Project started',
@@ -19,12 +42,13 @@ class Project extends Component {
     return (
       <div className="Project">
         <p className="App-intro">
-          This is the Project page.
+          This is the Project {"#" + this.props.location.query.id} page.
         </p>
+        <p>{project.name}</p>
         <p>
           {
-            timelineEntries.map((timelineEntry) => {
-              return <TimelineEntry {...timelineEntry}/>
+            timelineEntries.map((timelineEntry, idx) => {
+              return <TimelineEntry key={idx} {...timelineEntry}/>
             })
           }
         </p>
