ee355026a285d322659928c2a39fa25a836e008e
Maggie Caspar <mcaspar@mobify.com>
2016-09-17 21:34:26 -0700

    add add-project button

--- src/components/project-list/project-list.js
+++ src/components/project-list/project-list.js
@@ -41,7 +41,13 @@ class ProjectList extends Component {
           })
         }
         </ul>
-        <Link to="/project-setup">Project-setup</Link>
+        <div className="u-text-align-center">
+            <Link className="c-button c--add" to="/project-setup">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><style type="text/css">
+                </style><title>  Shape</title><path class="st0" d="M24 0C10.8 0 0 10.8 0 24s10.8 24 24 24 24-10.8 24-24S37.2 0 24 0zM36 26.4h-9.6V36h-4.8v-9.6H12v-4.8h9.6V12h4.8v9.6H36C36 21.6 36 26.4 36 26.4z"/></svg>
+            <div className="u-margin-top-md">Add Project</div>
+            </Link>
+        </div>
       </div>
     );
   }
