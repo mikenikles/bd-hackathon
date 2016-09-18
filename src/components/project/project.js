6d12ebb7a124e6aaab489ee5663ef4e4f2dd7a5d
Maggie Caspar <mcaspar@mobify.com>
2016-09-18 08:46:15 -0700

    style search for products button

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -55,7 +55,7 @@ class Project extends Component {
                 </div>
             </div>
         </section>
-        <section className="c-steps u-padding-all-lg">
+        <section className="c-steps u-padding-sides-lg">
             <h2>1. Research &amp; Ideation</h2>
             <p>Having a pla and collecting inspiration will make your project go much smoother.
             Think about what you like, what you will need and what skills and tools you will need.</p>
@@ -77,12 +77,12 @@ class Project extends Component {
                     <span className="u-padding-left-lg">Add Design</span>
                 </div>
             </div>
-            <h4>Notes</h4>
-            <p>A bunch of notes</p>
+            <h4 className="u-margin-bottom-sm">Notes</h4>
+            <textarea className="u-margin-bottom-xlg" placeholder="Measurements and other considerations"/>
             <hr></hr>
         </section>
-        <section className="c-steps u-padding-all-lg">
-            <h2>2. Decisions</h2>
+        <section className="c-steps u-padding-sides-lg u-padding-bottom-xlg">
+            <h2 className="u-margin-top-xlg">2. Decisions</h2>
             <p>Now you know what you want to accomplish, it&#39;s time to make some decisions.</p>
             <h4>What I need</h4>
             <label htmlFor="cbox1">Products</label>
@@ -100,11 +100,15 @@ class Project extends Component {
                 })
               }
             </ul>
-            <Link to={`/projects/${this.props.params.id}/search`}>Search for products and tools</Link>
-            <hr></hr>
+
+            <Link to={`/projects/${this.props.params.id}/search`} className="u-display-flex u--align-center u-margin-top-lg"><svg className="c-icon c--medium c--brand" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><style type="text/css">
+            </style><title>  Shape</title><path className="st0" d="M24 0C10.8 0 0 10.8 0 24s10.8 24 24 24 24-10.8 24-24S37.2 0 24 0zM36 26.4h-9.6V36h-4.8v-9.6H12v-4.8h9.6V12h4.8v9.6H36C36 21.6 36 26.4 36 26.4z"/></svg>
+            <span className="u-padding-left-lg">Search for products and tools</span></Link>
+
+            <hr className="u-margin-top-xxlg"></hr>
         </section>
-        <section className="c-steps u-padding-all-lg">
-            <h2>3. Chart Your Progress</h2>
+        <section className="c-steps u-padding-sides-lg">
+            <h2 className="u-margin-top-xlg">3. Chart Your Progress</h2>
           <section className="c-timeline">
               <div>
                   {
@@ -118,7 +122,7 @@ class Project extends Component {
         </section>
         <section className="c-steps u-padding-all-lg">
             <h2>4. Project Complete!</h2>
-            <button className="c-button c--primary c--full-width">I'M DONE</button>
+            <button className="c-button c--primary c--full-width">I&#39;M DONE</button>
             <h5>Actual Time to Complete</h5>
               <div className="u-display-flex u--space-between">
                 <label>1 &gt; Day</label><label>Weekend</label><label>Week</label><label>Month</label><label>3 Month</label><label>Year</label>
